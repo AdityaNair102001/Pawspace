6eace2d43710c057628897e9d9071ddd60cae0c9
krishnakant01 <63720159+krishnakant01@users.noreply.github.com>
2021-01-28 01:07:29 +0530

    Sign up Design

--- app/src/main/java/com/phoenixcorp/indiepaw/LoginActivity.java
+++ app/src/main/java/com/phoenixcorp/indiepaw/LoginActivity.java
@@ -2,7 +2,10 @@ package com.phoenixcorp.indiepaw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button signup = findViewById(R.id.SignUp);
+        signup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+                // Do something in response to button click
+            }
+        });
     }
+
 }
