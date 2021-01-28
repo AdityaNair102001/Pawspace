57f1aa00c0bb01cc98139950361e1d5164377131
AdityaNair102001 <adityanair102001@gmail.com>
2021-01-29 02:46:54 +0530

    Bottom Nav improvisation.

--- app/src/main/java/com/phoenixcorp/indiepaw/DefaultPageActivity.java
+++ app/src/main/java/com/phoenixcorp/indiepaw/DefaultPageActivity.java
@@ -1,8 +1,17 @@
 package com.phoenixcorp.indiepaw;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class DefaultPageActivity extends AppCompatActivity {
 
@@ -11,6 +20,11 @@ public class DefaultPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_default_page);
 
+    BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigationView);
+    bottomNavigationView.setBackground(null);
+    bottomNavigationView.getMenu().getItem(2).setEnabled(false);
+
 
     }
+
 }
