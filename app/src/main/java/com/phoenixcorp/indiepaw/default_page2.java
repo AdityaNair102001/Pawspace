aad453c261834360e45e46de9604d61ce273aff6
krishnakant01 <63720159+krishnakant01@users.noreply.github.com>
2021-03-16 00:36:58 +0530

    Default Page - Bottom Navigation-fab added

--- app/src/main/java/com/phoenixcorp/indiepaw/default_page2.java
+++ app/src/main/java/com/phoenixcorp/indiepaw/default_page2.java
@@ -7,26 +7,19 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class default_page2 extends AppCompatActivity {
 
-    BottomNavigationView bottomBar;
+    BottomAppBar bottomBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_default_page2);
 
-        bottomBar = findViewById(R.id.bottomBar);
+        bottomBar = findViewById(R.id.BAB);
+        bottomBar.setBackground(null);
 
-        bottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                Toast.makeText(default_page2.this, item.getTitle(), Toast.LENGTH_SHORT).show();
-
-                return true;
-            }
-        });
     }
 }
