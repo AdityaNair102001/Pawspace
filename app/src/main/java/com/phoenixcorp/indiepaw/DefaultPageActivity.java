1a5c1dca2bfe8f9905f20dda370327971256f5b1
AdityaNair102001 <adityanair102001@gmail.com>
2021-03-16 12:40:20 +0530

    Fragments Added.

--- app/src/main/java/com/phoenixcorp/indiepaw/DefaultPageActivity.java
+++ app/src/main/java/com/phoenixcorp/indiepaw/DefaultPageActivity.java
@@ -1,8 +1,11 @@
 package com.phoenixcorp.indiepaw;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,12 +16,42 @@ public class DefaultPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_default_page);
 
-        BottomNavigationView b = findViewById(R.id.bottomNavigationView);
-        b.setBackground(null);
-        b.getMenu().getItem(2).setEnabled(false);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.getMenu().getItem(2).setEnabled(false);
 
-
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer,new HomeFragment()).commit();
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener(){
+
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch(item.getItemId())
+            {
+                case R.id.home:
+                    selectedFragment=new HomeFragment();
+                    break;
+                    case R.id.search:
+                        selectedFragment=new SearchFragment();
+                        break;
+                    case R.id.blog:
+                        selectedFragment=new BlogFragment();
+                        break;
+                    case R.id.profile:
+                        selectedFragment=new ProfileFragment();
+                        break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer,selectedFragment).commit();
+            return true;
+        }
+    };
+
+
 }
