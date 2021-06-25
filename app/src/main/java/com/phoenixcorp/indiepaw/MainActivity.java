b0bc936a8d7e2454720f98a767d3fd697523546a
krishnakant01 <63720159+krishnakant01@users.noreply.github.com>
2021-06-26 02:04:00 +0530

    landing, login, sign up pages

--- app/src/main/java/com/phoenixcorp/indiepaw/MainActivity.java
+++ app/src/main/java/com/phoenixcorp/indiepaw/MainActivity.java
@@ -2,17 +2,62 @@ package com.phoenixcorp.indiepaw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5600;
+    Animation topAnim, bottomAnim;
+    GifImageView gif;
+    TextView appname, description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-        startActivity(intent);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+
+        gif = findViewById(R.id.gifImageView);
+        appname = findViewById(R.id.Pawspace);
+        description = findViewById(R.id.textV);
+
+        gif.setAnimation(topAnim);
+        appname.setAnimation(bottomAnim);
+        description.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(gif,"gifImageView2");
+                pairs[1] = new Pair<View,String>(appname,"text_animation");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent,options.toBundle());
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+
+
     }
 }
--- app/src/main/java/com/phoenixcorp/indiepaw/SignupActivity.java
+++ app/src/main/java/com/phoenixcorp/indiepaw/SignupActivity.java
@@ -2,19 +2,60 @@ package com.phoenixcorp.indiepaw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class SignupActivity extends AppCompatActivity {
+    Button signup, login;
+    GifImageView gif;
+    TextView signinText,greetings;
+    TextInputLayout email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        Button signup = findViewById(R.id.Login);
+        signup = findViewById(R.id.SignUp);
+        login = findViewById(R.id.Login);
+        gif = findViewById(R.id.gifImageView);
+        signinText = findViewById(R.id.textView2);
+        greetings = findViewById(R.id.Greetings);
+        email = findViewById(R.id.outlinedTextField2);
+        password = findViewById(R.id.outlinedTextField3);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                Pair[] pairs = new Pair[7];
+
+                pairs[0] = new Pair<View,String>(gif,"gifImageView2");
+                pairs[1] = new Pair<View,String>(greetings,"text_animation");
+                pairs[2] = new Pair<View,String>(signinText,"textview2_trans");
+                pairs[3] = new Pair<View,String>(email,"email_trans");
+                pairs[4] = new Pair<View,String>(password,"password_trans");
+                pairs[5] = new Pair<View,String>(login,"button1_trans");
+                pairs[6] = new Pair<View,String>(signup,"button2_trans");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignupActivity.this,pairs);
+                startActivity(intent,options.toBundle());
+                finish();
+            }
+        });
+
+
+
         signup.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
