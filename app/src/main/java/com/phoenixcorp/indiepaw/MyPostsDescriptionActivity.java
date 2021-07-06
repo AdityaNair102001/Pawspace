6e7290d37d160bf0773a7337285f23ae86113f7f
krishnakant01 <63720159+krishnakant01@users.noreply.github.com>
2021-07-06 22:04:22 +0530

    My Posts Grid View Implemented

---
+++ app/src/main/java/com/phoenixcorp/indiepaw/MyPostsDescriptionActivity.java
@@ -0,0 +1,14 @@
+package com.phoenixcorp.indiepaw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyPostsDescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_posts_description);
+    }
+}
